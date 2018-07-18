e2b749f067b94c50dbafb54ec80188f3dba8d3b0
Jeremy Greer <jeremy.greer@engageonq.com>
2018-07-18 09:05:27 -0400

    Number consoles, use error for better visibility

--- tests2/other-tests.js
+++ tests2/other-tests.js
@@ -2,7 +2,9 @@ require('jest');
 
 describe('bar', () => {
   it('always is true', () => {
-    console.log("JEST")
+    console.error("1 JEST")
+    console.error("2 JEST")
+    console.error("3 JEST")
     expect(1).toEqual(1)
   })
 })
